28283ff4fd4020722ac94c897909da2877380cbb
Kalidass2109 <dasskali057@gmail.com>
2022-12-17 23:45:49 +0530

    fourth commit

---
+++ src/com/java8/features/streams/StreamJoiningExample.java
@@ -0,0 +1,31 @@
+package com.java8.features.streams;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.java8.features.lambdas.Person;
+import com.java8.features.lambdas.PersonRepository;
+
+public class StreamJoiningExample {
+	
+	static String joiningExample() {
+		String s=PersonRepository.getAllPersons()
+				.stream()
+				.map(Person::getName)
+				//.collect(Collectors.joining());
+				//.collect(Collectors.joining("-"));
+				.collect(Collectors.joining("-", "[", "]"));
+		return s;
+	}
+
+	public static void main(String[] args) {
+		char[] ch= {'a','b','c','d','e'};
+		String join=Stream.of(ch)
+				.map(arr->new String(arr))
+				.collect(Collectors.joining("-","[","]"));
+		System.out.println("Joined Data :"+join);
+		System.out.println("Joining Output :"+joiningExample());
+
+	}
+
+}
